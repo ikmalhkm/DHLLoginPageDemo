4a0d2a86b02a627e02bd0128e73fc82428ead784
Ikmal Hakeem <asukiraijin@gmail.com>
2022-04-05 14:42:02 +0800

    Added my project

---
+++ Loginpage/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Loginpage.Models;
+
+namespace Loginpage.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public List<UserModel> PutValue()
+        {
+            var users = new List<UserModel>
+            {
+                new UserModel{id=1,username="ikmal",password="1234"},
+
+            };
+
+            return users;
+        }
+
+        [HttpPost]
+        public IActionResult Verify(UserModel usr)
+        {
+            var u = PutValue();
+
+            var ue = u.Where(u => u.username.Equals(usr.username));
+
+            var up = ue.Where(p => p.password.Equals(usr.password));
+
+            if(up.Count()==1)
+            {
+                ViewBag.message = "You deserve this funny video";
+                return View("LoginSuccess");
+            }
+            else
+            {
+                ViewBag.message = "Login Failed";
+                return View("Login");
+            }
+        }
+    }
+}
